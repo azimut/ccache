044ea1443c50b65d59e93fa81fa5ba05f4379311
azimut <azimut.github@protonmail.com>
2021-02-02 22:19:36 -0300

    MVP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate dirs;
 extern crate serde;
 extern crate serde_json;
 extern crate sha3;
@@ -5,9 +6,10 @@ extern crate sha3;
 use serde::{Deserialize, Serialize};
 use sha3::{Digest, Sha3_256};
 use std::env;
-// use std::fs;
+use std::fs;
 use std::fs::File;
-// use std::path::Path;
+use std::io::BufReader;
+use std::path::PathBuf;
 use std::process;
 use std::process::Command;
 
@@ -18,15 +20,9 @@ struct Item {
     args: String,
     stdout: String,
     stderr: String,
-    status: u8,
+    status: i32,
     filename: String,
-}
-
-fn hashit(v: &Vec<String>) -> String {
-    let args = v[1..].join(" ");
-    let data = args.as_bytes();
-    let hash = Sha3_256::digest(data);
-    format!("{:x}", hash)
+    path: PathBuf,
 }
 
 impl Item {
@@ -34,14 +30,17 @@ impl Item {
         let hash = hashit(&command);
         let args = command[1..].join(" ");
         let exe = command[0].clone();
+        let filename = format!("{}_{}.json", exe.clone(), hash.clone());
+        let path = datadir().join(filename.clone());
         Self {
             hash: hash.clone(),
             exe: exe.clone(),
             args: args,
-            stdout: "NO OUTPUT".to_string(),
-            stderr: "NO OUTPUT".to_string(),
-            status: 99,
-            filename: format!("{}_{}.json", exe.clone(), hash.clone()),
+            filename: filename.clone(),
+            path: path,
+            stdout: "".to_string(),
+            stderr: "".to_string(),
+            status: 0,
         }
     }
     fn execute(&mut self) {
@@ -51,28 +50,65 @@ impl Item {
             .expect("command failed to execute");
         self.stdout = String::from_utf8(output.stdout).unwrap();
         self.stderr = String::from_utf8(output.stderr).unwrap();
-        println!("{:?}", output.status);
+        self.status = output.status.code().unwrap();
     }
     fn save(&self) {
-        let file = File::create(&self.filename).expect("could not create the file");
+        let db = datadir();
+        mkdir(db);
+        let file = File::create(&self.path).expect("could not create the file");
         serde_json::to_writer_pretty(&file, &self).expect("could write the file");
+    }
+    fn find_backup(&self) -> Option<Item> {
+        if self.path.exists() {
+            let file = File::open(&self.path).expect("failed to open old");
+            let reader = BufReader::new(file);
+            let u = serde_json::from_reader(reader).expect("cannot deserialize");
+            Some(u)
+        } else {
+            None
+        }
+    }
+    fn replay(&self) {
+        println!("{}", self.stdout);
+        eprintln!("{}", self.stderr);
     }
 }
 
-// fn mkdir(s: &str) {
-//     if !Path::new(s).is_dir() {
-//         fs::create_dir(s).unwrap();
-//     }
-// }
+fn datadir() -> PathBuf {
+    let home = dirs::home_dir().unwrap();
+    home.join(".cache").join("ccache")
+}
+
+fn mkdir(s: PathBuf) {
+    if !s.exists() {
+        fs::create_dir(s).unwrap();
+    }
+}
+
+fn hashit(v: &Vec<String>) -> String {
+    let args = v.join(" ");
+    let data = args.as_bytes();
+    let hash = Sha3_256::digest(data);
+    format!("{:x}", hash)
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
         process::exit(1);
     }
-    let command = args[1..].into();
-    let mut item = Item::new(command);
-    println!("{}", item.hash.clone());
+
+    let mut item = Item::new(args[1..].into());
     item.execute();
-    item.save();
+    eprintln!("{}  {}", item.status, item.hash);
+    if item.status == 0 {
+        item.save();
+        item.replay();
+        process::exit(0);
+    }
+
+    match item.find_backup() {
+        Some(old) => old.replay(),
+        None => item.replay(),
+    }
 }
